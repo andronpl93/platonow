e30e578ad12f7afe22eca468ad56517dc10584a0
andronpl93 <free.2010@mail.ru>
2017-02-19 02:01:33 +0300

    картинки для шестиугольников

--- cube/static/js/hexahedron.js
+++ cube/static/js/hexahedron.js
@@ -1,12 +1,19 @@
-var hKol=0;
-var countPicture=10;
-var content=[['#74CDDB','Адаптивный дизайн'],
-             ['#7DE56F','Регистрация через соцсети'],
-             ['','<img src="../img/hex/">']];
+var settings = {
+     hKol          : 0,
+     content       : [
+                     '<img src="../../static/img/hex/ad.gif">',
+                     '<img src="../../static/img/hex/rs.gif">',
+                     '<img src="../../static/img/hex/pg.gif">',
+                     '<img src="../../static/img/hex/vk.gif">',
+                     '<img src="../../static/img/hex/fb.gif">',
+                     '<img src="../../static/img/hex/gp.gif">',
+                     '<img src="../../static/img/hex/ig.png">',
+                     '<img src="../../static/img/hex/ad2.gif">'
+                     ]
+     }
+
 res();
-
 window.onresize = hui;
-
 
 
 
@@ -19,32 +26,35 @@ function hui(){
     setTimeout(res,150);
 };
 
-
-
-
 function res(){
-  hKol=0;
+  settings.hKol=0;
   var cHei=$('section').height();
   var hexHei=$('.hex:first-child').height()+parseInt($('.hex:first-child').css('margin-top'));
-  var normHei=hexHei;
   var a=0;
-  var b = $('.hex:first-child').width()/(-2)-parseInt($('.hex:first-child').css('margin-right'))/2;
+  var marginLeft = $('.hex:first-child').width()/(-2)-parseInt($('.hex:first-child').css('margin-right'))/2;
 
-  $('.hex:first-child').css('margin-left',b+'px').children('.back').removeClass('back');
+
+  $('.back').each(function (index){
+        $(this).html(" ");
+        $(this).parent().css('background-color','black');
+  });
+
+  $('.hex:first-child').css('margin-left',marginLeft+'px').children('.back').removeClass('back');
   a=$('.hex:first-child').position().top;
 
    var index=0;
-   $('.hex:not(:first)').each(function (ind){
+   $('.hex:not(:first-child)').each(function (ind){
         if($(this).position().top!==a){
 
-           if($(this).position().top+$(this).height()+ parseInt($(this).css('margin-top')) < cHei)
+           if($(this).position().top+$(this).height()+ 2*parseInt($(this).css('margin-top')) < cHei)
             {
-                hKol-=1;
+                settings.hKol-=1;
+              //  $(this).css('background-color','green');
             }
             if(index)
             {
 
-                $(this).css({'clear':'left','margin-left':b+'px'}).children('.back').removeClass('back');
+                $(this).css({'clear':'left','margin-left':marginLeft+'px'}).children('.back').removeClass('back');
                 a=$(this).position().top;
                 index=0;
 
@@ -56,12 +66,74 @@ function res(){
                 index=1;
             }
         }
-        if($(this).position().top+$(this).height()+ parseInt($(this).css('margin-top'))< cHei)
-            hKol+=1
+        if($(this).position().top+$(this).height()+ 2*parseInt($(this).css('margin-top'))< cHei)
+        {
+            settings.hKol+=1;
+          //  $(this).css('background-color','red');
+        }
+        else
+            $(this).children('.back').removeClass('back');
 
    });
-
+    imagePlacement ();
+ //   alert(settings.hKol+" "+settings.content.length);
 };
+
+
+
+
+function imagePlacement (){
+    var mas=settings.content.slice();
+
+    var i=0;
+    $('.back').each(function (index){
+        if(i<settings.content.length){
+            if(index%Math.ceil(settings.hKol/(settings.hKol-settings.content.length))!=0){
+                $(this).html(randList(mas));
+                $(this).parent().css('background-color','transparent');
+                i++;
+            }
+
+        }
+    });
+
+}
+
+function randList(list){
+    var r,v;
+    while(true){
+        r=Math.floor(Math.random()*list.length);
+        if(list[r]!==undefined){
+            v = list[r];
+            list[r]=undefined;
+            return v;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
